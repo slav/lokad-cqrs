ff851dedcc1dbba6ddbeb3a3d2da27243dcc8557
Aygiz Kunafin <aygiz.kunafin@lokad.com>
2012-11-14 13:48:16 +0600

    add MessageSender and EnvelopeStreamer unit test

---
+++ Tools/SimpleTests/CQRS.Portable/MessageSenderTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Lokad.Cqrs;
+using Lokad.Cqrs.Envelope;
+using Lokad.Cqrs.Partition;
+using NUnit.Framework;
+
+namespace Sample.CQRS.Portable
+{
+    public class MessageSenderTest
+    {
+        private string _path;
+        [SetUp]
+        public void Setup()
+        {
+            _path = Path.Combine(Path.GetTempPath(), "MessageSender", Guid.NewGuid().ToString());
+            if (!Directory.Exists(_path))
+                Directory.CreateDirectory(_path);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (Directory.Exists(_path))
+                Directory.Delete(_path, true);
+        }
+
+        [Test]
+        public void when_send_message()
+        {
+            var serializer = new TestMessageSerializer(new[] { typeof(SerializerTest1), typeof(SerializerTest2), });
+            var streamer = new EnvelopeStreamer(serializer);
+            var queueWriter = new FileQueueWriter(new DirectoryInfo(_path), "test");
+            var sender = new MessageSender(streamer, queueWriter);
+            sender.Send(new SerializerTest1("Name1"), "EnvId", new[] { new MessageAttribute("key1", "val1"), new MessageAttribute("key2", "val2"), });
+            sender.Send(new SerializerTest1("Name1"), "EnvId");
+
+            Assert.AreEqual(2, Directory.GetFiles(_path).Length);
+        }
+    }
+}
--- Tools/SimpleTests/CQRS.Portable/MessageSerializerTest.cs
+++ Tools/SimpleTests/CQRS.Portable/MessageSerializerTest.cs
@@ -14,21 +14,21 @@ namespace Sample.CQRS.Portable
         [Test]
         public void when_write_and_read_message()
         {
-            var serializer = new TestMessageSerializer(new[] { typeof(Test1), typeof(Test2), });
-            var msg = new Test1() { Name = "test name" };
+            var serializer = new TestMessageSerializer(new[] { typeof(SerializerTest1), typeof(SerializerTest2), });
+            var msg = new SerializerTest1() { Name = "test name" };
             var stream = new MemoryStream();
             serializer.WriteMessage(msg, msg.GetType(), stream);
             stream.Seek(0, SeekOrigin.Begin);
             var readedMessage = serializer.ReadMessage(stream);
 
-            Assert.AreEqual(typeof(Test1), readedMessage.GetType());
-            Assert.AreEqual("test name", (readedMessage as Test1).Name);
+            Assert.AreEqual(typeof(SerializerTest1), readedMessage.GetType());
+            Assert.AreEqual("test name", (readedMessage as SerializerTest1).Name);
         }
 
         [Test]
         public void when_write_and_read_attributes()
         {
-            var serializer = new TestMessageSerializer(new[] { typeof(Test1), typeof(Test2), });
+            var serializer = new TestMessageSerializer(new[] { typeof(SerializerTest1), typeof(SerializerTest2), });
             var stream = new MemoryStream();
             serializer.WriteAttributes(new List<MessageAttribute> { new MessageAttribute("attr1", "val1"), new MessageAttribute("attr2", "val2") }, stream);
             stream.Seek(0, SeekOrigin.Begin);
@@ -44,7 +44,7 @@ namespace Sample.CQRS.Portable
         [Test]
         public void when_write_and_read_compactint()
         {
-            var serializer = new TestMessageSerializer(new[] { typeof(Test1), typeof(Test2), });
+            var serializer = new TestMessageSerializer(new[] { typeof(SerializerTest1), typeof(SerializerTest2), });
             var stream = new MemoryStream();
             serializer.WriteCompactInt(10, stream);
             stream.Seek(0, SeekOrigin.Begin);
@@ -56,8 +56,8 @@ namespace Sample.CQRS.Portable
         [Test]
         public void when_write_and_read_attributes_and_messages()
         {
-            var serializer = new TestMessageSerializer(new[] { typeof(Test1), typeof(Test2), });
-            var msg = new Test1() { Name = "test name" };
+            var serializer = new TestMessageSerializer(new[] { typeof(SerializerTest1), typeof(SerializerTest2), });
+            var msg = new SerializerTest1() { Name = "test name" };
             var stream = new MemoryStream();
             serializer.WriteAttributes(new List<MessageAttribute> { new MessageAttribute("attr1", "val1"), new MessageAttribute("attr2", "val2") }, stream);
             serializer.WriteMessage(msg, msg.GetType(), stream);
@@ -70,16 +70,16 @@ namespace Sample.CQRS.Portable
             Assert.AreEqual("val1", readedAttributes[0].Value);
             Assert.AreEqual("attr2", readedAttributes[1].Key);
             Assert.AreEqual("val2", readedAttributes[1].Value);
-            Assert.AreEqual(typeof(Test1), readedMessage.GetType());
-            Assert.AreEqual("test name", (readedMessage as Test1).Name);
+            Assert.AreEqual(typeof(SerializerTest1), readedMessage.GetType());
+            Assert.AreEqual("test name", (readedMessage as SerializerTest1).Name);
         }
 
         [Test]
         public void when_write_and_read_more_messages_with_attribute()
         {
-            var serializer = new TestMessageSerializer(new[] { typeof(Test1), typeof(Test2), });
-            var msg1 = new Test1() { Name = "message1" };
-            var msg2 = new Test2() { Name = "message2" };
+            var serializer = new TestMessageSerializer(new[] { typeof(SerializerTest1), typeof(SerializerTest2), });
+            var msg1 = new SerializerTest1() { Name = "message1" };
+            var msg2 = new SerializerTest2() { Name = "message2" };
             var stream = new MemoryStream();
             serializer.WriteAttributes(new List<MessageAttribute> { new MessageAttribute("attr1", "val1"), new MessageAttribute("attr2", "val2") }, stream);
             serializer.WriteCompactInt(2, stream);
@@ -96,30 +96,10 @@ namespace Sample.CQRS.Portable
             Assert.AreEqual("val1", readedAttributes[0].Value);
             Assert.AreEqual("attr2", readedAttributes[1].Key);
             Assert.AreEqual("val2", readedAttributes[1].Value);
-            Assert.AreEqual(typeof(Test1), readedMessage1.GetType());
-            Assert.AreEqual("message1", (readedMessage1 as Test1).Name);
-            Assert.AreEqual(typeof(Test2), readedMessage2.GetType());
-            Assert.AreEqual("message2", (readedMessage2 as Test2).Name);
-        }
-
-        class Test1
-        {
-            private int Id { get; set; }
-            public string Name { get; set; }
-        }
-
-        class Test2
-        {
-            private int Id { get; set; }
-            public string Name { get; set; }
-
-            public void Method1()
-            { }
-
-            public int Method2()
-            {
-                return 1;
-            }
+            Assert.AreEqual(typeof(SerializerTest1), readedMessage1.GetType());
+            Assert.AreEqual("message1", (readedMessage1 as SerializerTest1).Name);
+            Assert.AreEqual(typeof(SerializerTest2), readedMessage2.GetType());
+            Assert.AreEqual("message2", (readedMessage2 as SerializerTest2).Name);
         }
     }
 
@@ -145,4 +125,30 @@ namespace Sample.CQRS.Portable
         }
     }
 
+    public class SerializerTest1
+    {
+        private int Id { get; set; }
+        public string Name { get; set; }
+
+        public SerializerTest1()
+        {}
+        public SerializerTest1(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class SerializerTest2
+    {
+        private int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Method1()
+        { }
+
+        public int Method2()
+        {
+            return 1;
+        }
+    }
 }
