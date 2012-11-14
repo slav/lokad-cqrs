9e552658e3bf45036f5b5bd772f3b993c0ff2856
Aygiz Kunafin <aygiz.kunafin@lokad.com>
2012-11-14 11:27:02 +0600

    add unit test for ImmutableEnvelope

---
+++ Tools/SimpleTests/CQRS.Portable/MessageSerializerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Lokad.Cqrs.Envelope;
+using Lokad.Cqrs.Evil;
+using NUnit.Framework;
+using ServiceStack.Text;
+
+namespace Sample.CQRS.Portable
+{
+    public class MessageSerializerTest
+    {
+        [Test]
+         public void when_write_message()
+         {
+             
+         }
+    }
+
+    public class TestMessageSerializer:AbstractMessageSerializer
+    {
+        public TestMessageSerializer(ICollection<Type> knownTypes) : base(knownTypes)
+        {
+        }
+
+        protected override Formatter PrepareFormatter(Type type)
+        {
+            var name = ContractEvil.GetContractReference(type);
+            return new Formatter(name, type, s => JsonSerializer.DeserializeFromStream(type, s), (o, s) =>
+            {
+                using (var writer = new StreamWriter(s))
+                {
+                    writer.WriteLine();
+                    writer.WriteLine(JsvFormatter.Format(JsonSerializer.SerializeToString(o, type)));
+                }
+
+            });
+        }
+    }
+}
