180c7152f71f50b1f827320e14b6302cf8021709
Aygiz Kunafin <aygiz.kunafin@lokad.com>
2012-11-19 14:37:32 +0600

    add unit test for TypedMessageSender

---
+++ Tools/SimpleTests/CQRS.Portable/EnvelopeQuarantineTest.cs
@@ -0,0 +1,19 @@
+﻿using Lokad.Cqrs;
+using Lokad.Cqrs.Envelope;
+using NUnit.Framework;
+using SaaS.Wires;
+
+namespace Sample.CQRS.Portable
+{
+    public class EnvelopeQuarantineTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            var serializer = new TestMessageSerializer(new[] { typeof(SerializerTest1), typeof(SerializerTest2), });
+            IEnvelopeStreamer streamer = new EnvelopeStreamer(serializer);
+            //TypedMessageSender writer = 
+            //, IStreamContainer root
+        }
+    }
+}
