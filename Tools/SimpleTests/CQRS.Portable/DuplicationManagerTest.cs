3fce7bf15347828fb6d35cf46011e868efee1b48
Aygiz Kunafin <aygiz.kunafin@lokad.com>
2012-11-21 11:41:08 +0600

    add unit test for DuplicationManager

---
+++ Tools/SimpleTests/CQRS.Portable/DuplicationManagerTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using Lokad.Cqrs.Envelope;
+using NUnit.Framework;
+
+namespace Sample.CQRS.Portable
+{
+    public class DuplicationManagerTest
+    {
+        [Test]
+        public void when_get()
+        {
+            var manager = new DuplicationManager();
+            var memory1 = manager.GetOrAdd("dispatcher");
+            var memory2 = manager.GetOrAdd("dispatcher");
+
+            Assert.AreEqual(memory1, memory2);
+        }
+    }
+}
