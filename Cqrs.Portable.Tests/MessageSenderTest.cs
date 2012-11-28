a89921b7c8cee13b5e363b54ae620903cdac70dd
Aygiz Kunafin <aygiz.kunafin@lokad.com>
2012-11-28 17:00:07 +0600

    add specification with empty directory

--- Cqrs.Portable.Tests/MessageSenderTest.cs
+++ Cqrs.Portable.Tests/MessageSenderTest.cs
@@ -8,35 +8,19 @@ using NUnit.Framework;
 
 namespace Cqrs.Portable.Tests
 {
-    public class MessageSenderTest
+    public class MessageSenderTest : specification_with_empty_directory
     {
-        private string _path;
-        [SetUp]
-        public void Setup()
-        {
-            _path = Path.Combine(Path.GetTempPath(), "MessageSender", Guid.NewGuid().ToString());
-            if (!Directory.Exists(_path))
-                Directory.CreateDirectory(_path);
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            if (Directory.Exists(_path))
-                Directory.Delete(_path, true);
-        }
-
-        [Test]
+       [Test]
         public void when_send_message()
         {
             var serializer = new TestMessageSerializer(new[] { typeof(SerializerTest1), typeof(SerializerTest2), });
             var streamer = new EnvelopeStreamer(serializer);
-            var queueWriter = new FileQueueWriter(new DirectoryInfo(_path), "test");
+            var queueWriter = new FileQueueWriter(new DirectoryInfo(DirectoryPath), "test");
             var sender = new MessageSender(streamer, queueWriter);
             sender.Send(new SerializerTest1("Name1"), "EnvId", new[] { new MessageAttribute("key1", "val1"), new MessageAttribute("key2", "val2"), });
             sender.Send(new SerializerTest1("Name1"), "EnvId");
 
-            Assert.AreEqual(2, Directory.GetFiles(_path).Length);
+            Assert.AreEqual(2, Directory.GetFiles(DirectoryPath).Length);
         }
     }
 }
